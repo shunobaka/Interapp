3778c7e61ab5028289fe51298bf1912651933c1f
shunobaka <vesselin_minev@abv.bg>
2016-02-04 11:18:41 +0200

    Extracted dbcontext into data

---
+++ Source/Data/Interapp.Data/InterappDbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace Interapp.Data
+{
+    using Models;
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    public class InterappDbContext : IdentityDbContext<User>
+    {
+        public InterappDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static InterappDbContext Create()
+        {
+            return new InterappDbContext();
+        }
+    }
+}
