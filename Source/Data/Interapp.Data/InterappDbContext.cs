b528d2d74c9d3d23ff6d255d5f4c515f1db169c7
shunobaka <vesselin_minev@abv.bg>
2016-02-04 13:21:32 +0200

    Added models to context

--- Source/Data/Interapp.Data/InterappDbContext.cs
+++ Source/Data/Interapp.Data/InterappDbContext.cs
@@ -2,6 +2,7 @@
 {
     using Microsoft.AspNet.Identity.EntityFramework;
     using Models;
+    using System.Data.Entity;
 
     public class InterappDbContext : IdentityDbContext<User>
     {
@@ -9,6 +10,18 @@
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
+
+        public IDbSet<Country> Countries { get; set; }
+
+        public IDbSet<Document> Documents { get; set; }
+
+        public IDbSet<Essay> Essays { get; set; }
+
+        public IDbSet<Major> Majors { get; set; }
+
+        public IDbSet<Score> Scores { get; set; }
+
+        public IDbSet<University> Universities { get; set; }
 
         public static InterappDbContext Create()
         {
