2d093f895313c0d986805e404f6b7715e0627346
shunobaka <vesselin_minev@abv.bg>
2016-02-04 11:23:37 +0200

    Extended user model to hold general person information
    included firstname, lastname, date of birth

--- Source/Data/Interapp.Data.Models/User.cs
+++ Source/Data/Interapp.Data.Models/User.cs
@@ -2,11 +2,18 @@
 {
     using Microsoft.AspNet.Identity;
     using Microsoft.AspNet.Identity.EntityFramework;
+    using System;
     using System.Security.Claims;
     using System.Threading.Tasks;
 
     public class User : IdentityUser
     {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+        
+        public DateTime DateOfBrith { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<User> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
