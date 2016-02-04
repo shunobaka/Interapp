4491d72bba9c92c6a5aa3e5f90940f44444be08e
shunobaka <vesselin_minev@abv.bg>
2016-02-04 12:36:23 +0200

    Extended user model to have country

--- Source/Data/Interapp.Data.Models/User.cs
+++ Source/Data/Interapp.Data.Models/User.cs
@@ -4,6 +4,7 @@
     using Microsoft.AspNet.Identity.EntityFramework;
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Security.Claims;
     using System.Threading.Tasks;
@@ -39,6 +40,12 @@
 
         [ForeignKey("MajorId")]
         public virtual Major Major { get; set; }
+
+        [Required]
+        public int CountryId { get; set; }
+
+        [ForeignKey("CountryId")]
+        public virtual Country Country { get; set; }
 
         public virtual ICollection<Application> Applications
         {
