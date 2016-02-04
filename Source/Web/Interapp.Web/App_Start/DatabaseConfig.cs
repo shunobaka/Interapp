3648baba0c416905e784da1b01fc7f48f92cf860
shunobaka <vesselin_minev@abv.bg>
2016-02-04 16:34:08 +0200

    Fixed connection string and added db config file initialized in global.asax

---
+++ Source/Web/Interapp.Web/App_Start/DatabaseConfig.cs
@@ -0,0 +1,15 @@
+﻿namespace Interapp.Web
+{
+    using Data;
+    using Data.Migrations;
+    using System.Data.Entity;
+
+    public class DatabaseConfig
+    {
+        public static void Initialize()
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<InterappDbContext, Configuration>());
+            InterappDbContext.Create().Database.Initialize(true);
+        }
+    }
+}
--- Source/Web/Interapp.Web/Global.asax.cs
+++ Source/Web/Interapp.Web/Global.asax.cs
@@ -16,6 +16,7 @@ namespace Interapp.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            DatabaseConfig.Initialize();
         }
     }
 }
