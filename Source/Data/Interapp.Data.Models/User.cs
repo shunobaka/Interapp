f9423e8ef66a5afbda203bec27be92820de9245a
shunobaka <vesselin_minev@abv.bg>
2016-02-04 11:55:58 +0200

    Implemented university application model

--- Source/Data/Interapp.Data.Models/User.cs
+++ Source/Data/Interapp.Data.Models/User.cs
@@ -3,16 +3,37 @@
     using Microsoft.AspNet.Identity;
     using Microsoft.AspNet.Identity.EntityFramework;
     using System;
+    using System.Collections.Generic;
     using System.Security.Claims;
     using System.Threading.Tasks;
 
     public class User : IdentityUser
     {
+        private ICollection<Application> applications;
+
+        public User()
+        {
+            this.applications = new HashSet<Application>();
+        }
+
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
         
         public DateTime DateOfBrith { get; set; }
+
+        public virtual ICollection<Application> Applications
+        {
+            get
+            {
+                return this.applications;
+            }
+
+            set
+            {
+                this.applications = value;
+            }
+        }
 
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<User> manager)
         {
