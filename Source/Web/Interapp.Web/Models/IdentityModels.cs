1a41a6bdca6ddeb300905e0af924e68ddde6e3c2
shunobaka <vesselin_minev@abv.bg>
2016-02-04 11:15:36 +0200

    Fix compilation errors regarding not found user

--- Source/Web/Interapp.Web/Models/IdentityModels.cs
+++ Source/Web/Interapp.Web/Models/IdentityModels.cs
@@ -1,14 +1,11 @@
-﻿using System.Data.Entity;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Interapp.Data.Models;
 
 namespace Interapp.Web.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    public class ApplicationDbContext : IdentityDbContext<User>
     {
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
