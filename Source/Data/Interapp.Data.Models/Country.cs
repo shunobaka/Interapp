3fae828a98e3ae7f58cd3a84707514df6021ba53
shunobaka <vesselin_minev@abv.bg>
2016-02-04 11:28:40 +0200

    Implemented model for country

---
+++ Source/Data/Interapp.Data.Models/Country.cs
@@ -0,0 +1,34 @@
+﻿namespace Interapp.Data.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Country
+    {
+        private ICollection<User> users;
+
+        public Country()
+        {
+            this.users = new HashSet<User>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public virtual ICollection<User> Users
+        {
+            get
+            {
+                return this.users;
+            }
+
+            set
+            {
+                this.users = value;
+            }
+        }
+    }
+}
