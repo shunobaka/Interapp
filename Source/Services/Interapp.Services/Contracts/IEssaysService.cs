96ebf52011e8b604281de7e1eb1ea2501af219b6
shunobaka <vesselin_minev@abv.bg>
2016-02-05 12:51:24 +0200

    Implemented interface for EssaysService

---
+++ Source/Services/Interapp.Services/Contracts/IEssaysService.cs
@@ -0,0 +1,15 @@
+﻿namespace Interapp.Services.Contracts
+{
+    using Data.Models;
+
+    public interface IEssaysService
+    {
+        Essay GetByStudentId(string studentId);
+
+        void Create(string studentId, string title, string content);
+
+        void Update(string studentId, string title, string content);
+
+        void Delete(string studentId);
+    }
+}
