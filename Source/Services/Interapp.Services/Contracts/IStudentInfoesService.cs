9f40d4ee35ab07b580e6e2edace1df39f02b4e0c
shunobaka <vesselin_minev@abv.bg>
2016-02-05 12:36:41 +0200

    Created interface for StudentInfoesService

---
+++ Source/Services/Interapp.Services/Contracts/IStudentInfoesService.cs
@@ -0,0 +1,17 @@
+﻿namespace Interapp.Services.Contracts
+{
+    using Data.Models;
+
+    public interface IStudentInfoesService
+    {
+        StudentInfo GetById(int id);
+
+        void Update(string studentId, StudentInfo info);
+
+        void AddUniversityOfInterest(University university);
+
+        void EnrollStudent(string studentId, int universityId, int majorId);
+
+        void Create(string studentId, int universityId, int majorId);
+    }
+}
