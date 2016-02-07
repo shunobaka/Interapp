63b2d24a40247b5b42adb958e07217727d90c78b
shunobaka <vesselin_minev@abv.bg>
2016-02-07 21:53:27 +0200

    Added controller and simple review view for scores

---
+++ Source/Web/Interapp.Web/Areas/Student/Controllers/ScoresController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Interapp.Web.Areas.Student.Controllers
+{
+    public class ScoresController : Controller
+    {
+        public ActionResult Review()
+        {
+            return View();
+        }
+    }
+}
