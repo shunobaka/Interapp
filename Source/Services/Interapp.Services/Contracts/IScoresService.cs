60872cebcb997c147458371ff2fa429d1e0040f3
shunobaka <vesselin_minev@abv.bg>
2016-02-07 10:38:30 +0200

    Implemented scores service

---
+++ Source/Services/Interapp.Services/Contracts/IScoresService.cs
@@ -0,0 +1,15 @@
+﻿namespace Interapp.Services.Contracts
+{
+    using Data.Models;
+
+    public interface IScoresService
+    {
+        ScoreReport GetByStudentId(string studentId);
+
+        void Create(ScoreReport scores);
+
+        void Update(ScoreReport scores);
+
+        void Delete(string studentId);
+    }
+}
---
+++ Source/Services/Interapp.Services/ScoresService.cs
@@ -0,0 +1,45 @@
+﻿namespace Interapp.Services
+{
+    using Contracts;
+    using System.Linq;
+    using Data.Models;
+    using System;
+    using Data.Repositories;
+
+    public class ScoresService : IScoresService
+    {
+        private IRepository<ScoreReport> scores;
+
+        public ScoresService(IRepository<ScoreReport> scores)
+        {
+            this.scores = scores;
+        }
+
+        public void Create(ScoreReport newScores)
+        {
+            this.scores.Add(newScores);
+            this.scores.SaveChanges();
+        }
+
+        public void Delete(string studentId)
+        {
+            this.scores.Delete(studentId);
+        }
+
+        public ScoreReport GetByStudentId(string studentId)
+        {
+            var scoresForStudent = this.scores
+                .All()
+                .Where(s => s.StudentInfoId == studentId)
+                .FirstOrDefault();
+
+            return scoresForStudent;
+        }
+
+        public void Update(ScoreReport newScores)
+        {
+            this.scores.Update(newScores);
+            this.scores.SaveChanges();
+        }
+    }
+}
