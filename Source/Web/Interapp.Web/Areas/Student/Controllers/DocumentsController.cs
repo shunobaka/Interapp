48eb268eacd659b30827967602d860f28fde20f5
shunobaka <vesselin_minev@abv.bg>
2016-02-08 17:47:02 +0200

    Fixed model for all documents view

--- Source/Web/Interapp.Web/Areas/Student/Controllers/DocumentsController.cs
+++ Source/Web/Interapp.Web/Areas/Student/Controllers/DocumentsController.cs
@@ -4,6 +4,7 @@
     using Interapp.Services.Contracts;
     using Microsoft.AspNet.Identity;
     using Models.DocumentViewModels;
+    using Models.UniversityViewModels;
     using System.Linq;
     using System.Web.Mvc;
 
@@ -11,26 +12,39 @@
     {
         private IDocumentsService documents;
         private IUniversitiesService universities;
+        private IStudentInfosService students;
 
-        public DocumentsController(IDocumentsService documents, IUniversitiesService universities)
+        public DocumentsController(IDocumentsService documents, IUniversitiesService universities, IStudentInfosService students)
         {
+            this.students = students;
             this.documents = documents;
             this.universities = universities;
         }
 
         public ActionResult All()
         {
-            var studentId = this.User.Identity.GetUserId();
-            var studentDocuments = this.documents
-                .GetByStudent(studentId)
-                .ProjectTo<DocumentViewModel>()
-                .ToList();
+            //var studentId = this.User.Identity.GetUserId();
+            //var studentDocuments = this.documents
+            //    .GetByStudent(studentId)
+            //    .ProjectTo<DocumentViewModel>()
+            //    .ToList();
 
-            var universities = this.universities
-                .GetForUserWithDocuments(studentId);
+            //var universities = this.universities
+            //    .GetForUserWithDocuments(studentId);
             // TODO: Finish and add model to view
+            var studentId = this.User.Identity.GetUserId();
+            var studentInfo = this.students.GetFullInfoById(studentId);
 
-            return View();
+            var studentDocuments = studentInfo.Documents.AsQueryable().ProjectTo<DocumentViewModel>().ToList();
+            var universities = studentInfo.UniversitiesOfInterest.AsQueryable().ProjectTo<UniversityViewModel>().ToList();
+
+            var model = new DocumentsFullViewModel()
+            {
+                StudentDocuments = studentDocuments,
+                Universities = universities
+            };
+
+            return View(model);
         }
     }
 }
