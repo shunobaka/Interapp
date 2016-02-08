56a08f5964c1cb1c13a1c912916191a719d999fa
shunobaka <vesselin_minev@abv.bg>
2016-02-08 13:03:46 +0200

    Implemented DocumentsService

---
+++ Source/Services/Interapp.Services/Contracts/IDocumentsService.cs
@@ -0,0 +1,18 @@
+﻿namespace Interapp.Services.Contracts
+{
+    using Data.Models;
+    using System.Linq;
+
+    public interface IDocumentsService
+    {
+        IQueryable<Document> GetByUniversity(int universityId);
+
+        IQueryable<Document> GetByStudent(string studentId);
+
+        void CreateForStudent(string studentId, string name, string content);
+
+        void CreateForUniversity(int universityId, string name, string content);
+
+        void Delete(int documentId);
+    }
+}
