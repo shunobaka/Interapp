67f42796fbbfb8b267398b0aa9a4727df1cdef83
shunobaka <vesselin_minev@abv.bg>
2016-02-08 13:14:26 +0200

    Created simple view with controller for displaying documents

--- Source/Web/Interapp.Web/Areas/Student/Controllers/DocumentsController.cs
+++ Source/Web/Interapp.Web/Areas/Student/Controllers/DocumentsController.cs
@@ -1,18 +1,30 @@
-﻿using System.Web.Mvc;
-
-namespace Interapp.Web.Areas.Student.Controllers
+﻿namespace Interapp.Web.Areas.Student.Controllers
 {
+    using AutoMapper.QueryableExtensions;
+    using Interapp.Services.Contracts;
+    using Microsoft.AspNet.Identity;
+    using Models.DocumentViewModels;
+    using System.Linq;
+    using System.Web.Mvc;
+
     public class DocumentsController : Controller
     {
+        private IDocumentsService documents;
 
-        public DocumentsController()
+        public DocumentsController(IDocumentsService documents)
         {
-
+            this.documents = documents;
         }
 
         public ActionResult All()
         {
-            return View();
+            var studentId = this.User.Identity.GetUserId();
+            var model = this.documents
+                .GetByStudent(studentId)
+                .ProjectTo<DocumentViewModel>()
+                .ToList();
+
+            return View(model);
         }
     }
 }
