0d18673c50af7d6044beb43390416edc91714d0c
shunobaka <vesselin_minev@abv.bg>
2016-02-08 12:54:11 +0200

    Added DocumentsController and simple DocumentViewModel

---
+++ Source/Web/Interapp.Web/Areas/Student/Controllers/DocumentsController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace Interapp.Web.Areas.Student.Controllers
+{
+    public class DocumentsController : Controller
+    {
+
+        public DocumentsController()
+        {
+
+        }
+
+        public ActionResult All()
+        {
+            return View();
+        }
+    }
+}
---
+++ Source/Web/Interapp.Web/Areas/Student/Models/DocumentViewModels/DocumentViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Interapp.Web.Areas.Student.Models.DocumentViewModels
+{
+    using System.ComponentModel.DataAnnotations;
+    using Common.Constants;
+    using Data.Models;
+    using Infrastructure.Mappings;
+
+    public class DocumentViewModel : IMapFrom<Document>
+    {
+        [Required]
+        [MinLength(ModelConstants.DocumentNameMinLength)]
+        [MaxLength(ModelConstants.DocumentNameMaxLength)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+    }
+}
