3d9e3630e0656d47bb5f01e0090b8b4e979200e7
shunobaka <vesselin_minev@abv.bg>
2016-02-09 13:54:11 +0200

    Implemented CountryViewModel

---
+++ Source/Web/Interapp.Web/Models/CountryModels/CountryViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Interapp.Web.Models.CountryModels
+{
+    using Common.Constants;
+    using System.ComponentModel.DataAnnotations;
+
+    public class CountryViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(ModelConstants.CountryNameMinLength)]
+        [MaxLength(ModelConstants.CountryNameMaxLenght)]
+        public string Name { get; set; }
+    }
+}
