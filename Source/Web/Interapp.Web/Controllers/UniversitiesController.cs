13a4442c27fd42d8b0278cb4de11ea89a1de7e0f
shunobaka <vesselin_minev@abv.bg>
2016-02-09 19:01:27 +0200

    Implemented simple UniversitiesController with filtering

---
+++ Source/Web/Interapp.Web/Controllers/UniversitiesController.cs
@@ -0,0 +1,42 @@
+﻿namespace Interapp.Web.Controllers
+{
+    using AutoMapper.QueryableExtensions;
+    using Data.Models;
+    using Interapp.Services.Common;
+    using Interapp.Services.Contracts;
+    using Models.UniversityViewModels;
+    using System;
+    using System.Linq;
+    using System.Web.Caching;
+    using System.Web.Mvc;
+
+    public class UniversitiesController : Controller
+    {
+        private IUniversitiesService universities;
+
+        public UniversitiesController(IUniversitiesService universities)
+        {
+            this.universities = universities;
+        }
+
+        public ActionResult All(FilterModel filter)
+        {
+            if (this.HttpContext.Cache["Universities"] == null)
+            {
+                var unis = this.universities.All();
+                this.HttpContext.Cache.Add("Countries",
+                    unis,
+                    null,
+                    DateTime.Now.AddHours(1),
+                    TimeSpan.Zero,
+                    CacheItemPriority.Default, null);
+            }
+
+            var model = this.universities
+                .FilterUniversities((IQueryable<University>)this.HttpContext.Cache["Universities"], filter)
+                .ProjectTo<UniversityViewModel>();
+
+            return View(model);
+        }
+    }
+}
