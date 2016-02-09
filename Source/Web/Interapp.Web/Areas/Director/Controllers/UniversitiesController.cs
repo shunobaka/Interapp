1d2fcaa65b153ad2f9af4de3bfd654858be8c142
shunobaka <vesselin_minev@abv.bg>
2016-02-09 10:48:50 +0200

    Implemented simple view with validation for adding new university

---
+++ Source/Web/Interapp.Web/Areas/Director/Controllers/UniversitiesController.cs
@@ -0,0 +1,21 @@
+﻿namespace Interapp.Web.Areas.Director.Controllers
+{
+    using Models;
+    using System;
+    using System.Web.Mvc;
+
+    public class UniversitiesController : Controller
+    {
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(UniversityCreateViewModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Source/Web/Interapp.Web/Areas/Director/Models/UniversityCreateViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Interapp.Web.Areas.Director.Models
+{
+    using Common.Constants;
+    using System.ComponentModel.DataAnnotations;
+
+    public class UniversityCreateViewModel
+    {
+        [Required]
+        [Display(Name = "University name")]
+        [MinLength(ModelConstants.UniversityNameMinLength, ErrorMessage = ModelErrorMessages.MinLengthErrorMessage)]
+        [MaxLength(ModelConstants.UniversityNameMaxLength, ErrorMessage = ModelErrorMessages.MinLengthErrorMessage)]
+        [RegularExpression(ModelConstants.UniversityNameRegex)]
+        public string Name { get; set; }
+
+        [Display(Name = "Tuition fee")]
+        [Range(ModelConstants.UniversityTuitionFeeMin, ModelConstants.UniversityTuitionFeeMax, ErrorMessage = "{0} must be between {1:C} and {2:C}")]
+        public int TuitionFee { get; set; }
+    }
+}
