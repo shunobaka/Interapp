ffa11457eb8bccfd91c739c0def23826442a3d45
shunobaka <vesselin_minev@abv.bg>
2016-02-09 10:10:44 +0200

    Created default director area with namespace for routes

---
+++ Source/Web/Interapp.Web/Areas/Director/DirectorAreaRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace Interapp.Web.Areas.Director
+{
+    public class DirectorAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Director";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Director_default",
+                "Director/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional },
+                new string[] { "Director.Controllers" }
+            )
+            .DataTokens["UseNamespaceFallback"] = true;
+        }
+    }
+}
