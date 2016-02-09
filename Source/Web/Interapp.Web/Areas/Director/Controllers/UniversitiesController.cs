2724b8c75a33f172b1f1fd5019909a1b8feac3e8
shunobaka <vesselin_minev@abv.bg>
2016-02-09 10:50:34 +0200

    Started implemented creation of university logic

--- Source/Web/Interapp.Web/Areas/Director/Controllers/UniversitiesController.cs
+++ Source/Web/Interapp.Web/Areas/Director/Controllers/UniversitiesController.cs
@@ -1,11 +1,19 @@
 ﻿namespace Interapp.Web.Areas.Director.Controllers
 {
     using Models;
+    using Services.Contracts;
     using System;
     using System.Web.Mvc;
 
     public class UniversitiesController : Controller
     {
+        private IUniversitiesService universities;
+
+        public UniversitiesController(IUniversitiesService universities)
+        {
+            this.universities = universities;
+        }
+
         [HttpGet]
         public ActionResult Add()
         {
@@ -15,7 +23,12 @@
         [HttpPost]
         public ActionResult Add(UniversityCreateViewModel model)
         {
-            throw new NotImplementedException();
+            if (this.ModelState.IsValid)
+            {
+                //var directorId = this.User.Identity.GetUserId();
+                //this.universities.Create(directorId, model.)
+                // TODO: Implement
+            }
         }
     }
 }
