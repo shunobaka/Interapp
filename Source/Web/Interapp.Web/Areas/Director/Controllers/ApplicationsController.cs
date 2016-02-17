ade77a383f3de3e0f844702cd285113f2488b1fc
shunobaka <vesselin_minev@abv.bg>
2016-02-17 17:22:54 +0200

    Implemented logic and view models for application details

--- Source/Web/Interapp.Web/Areas/Director/Controllers/ApplicationsController.cs
+++ Source/Web/Interapp.Web/Areas/Director/Controllers/ApplicationsController.cs
@@ -5,6 +5,8 @@
     using Microsoft.AspNet.Identity;
     using Services.Contracts;
     using System.Web.Mvc;
+    using System.Linq;
+    using AutoMapper;
 
     [Authorize(Roles = "Director")]
     public class ApplicationsController : Controller
@@ -24,6 +26,23 @@
                 .ProjectTo<ApplicationViewModel>();
 
             return View(model);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var application = this.applications
+                .GetById(id);
+
+            var directorId = this.User.Identity.GetUserId();
+
+            if (application == null || application.University.DirectorId != directorId)
+            {
+                return this.View();
+            }
+
+            var model = Mapper.Map<ApplicationDetailsViewModel>(application);
+
+            return this.View();
         }
     }
 }
--- Source/Web/Interapp.Web/Areas/Director/Models/ApplicationsViewModels/ApplicationDetailsViewModel.cs
+++ Source/Web/Interapp.Web/Areas/Director/Models/ApplicationsViewModels/ApplicationDetailsViewModel.cs
@@ -4,20 +4,20 @@
     using Data.Models;
     using Infrastructure.Mappings;
 
-    public class ApplicationViewModel : IMapFrom<Application>
+    public class ApplicationDetailsViewModel : IMapFrom<Application>
     {
         public int Id { get; set; }
 
         public DateTime? DateCreated { get; set; }
 
         public bool IsAnswered { get; set; }
-        
+
         public int UniversityId { get; set; }
-        
+
         public UniversityViewModel University { get; set; }
 
-        public StudentInfoViewModel Student { get; set; }
-        
+        public StudentInfoDetailsViewModel Student { get; set; }
+
         public MajorViewModel Major { get; set; }
 
         public int? ResponseId { get; set; }
