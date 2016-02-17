3d10f466a0baa3c229f1c58260610533c07ad503
shunobaka <vesselin_minev@abv.bg>
2016-02-17 16:28:21 +0200

    Created simple ApplicationsController

---
+++ Source/Web/Interapp.Web/Areas/Director/Controllers/ApplicationsController.cs
@@ -0,0 +1,21 @@
+﻿namespace Interapp.Web.Areas.Director.Controllers
+{
+    using Services.Contracts;
+    using System.Web.Mvc;
+
+    [Authorize(Roles = "Director")]
+    public class ApplicationsController : Controller
+    {
+        private IApplicationsService applications;
+
+        public ApplicationsController(IApplicationsService applications)
+        {
+            this.applications = applications;
+        }
+
+        public ActionResult All()
+        {
+            return View();
+        }
+    }
+}
