d421cdabc0538a9a32589e3abcfb7677eed846a4
shunobaka <vesselin_minev@abv.bg>
2016-02-17 19:23:33 +0200

    Implemented view models for responses

---
+++ Source/Web/Interapp.Web/Areas/Student/Models/ResponsesViewModels/ResponseViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Interapp.Web.Areas.Student.Models.ResponsesViewModels
+{
+    using Data.Models;
+    using Infrastructure.Mappings;
+
+    public class ResponseViewModel : IMapFrom<Response>
+    {
+        public string Content { get; set; }
+
+        public bool IsAdmitted { get; set; }
+
+        public ApplicationViewModel Application { get; set; }
+    }
+}
