da821762a4d043cfec6812cd160914392afc3a9e
shunobaka <vesselin_minev@abv.bg>
2016-02-17 16:25:51 +0200

    Implemented necessary view models for applications

---
+++ Source/Web/Interapp.Web/Areas/Director/Models/ApplicationsViewModels/MajorViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Interapp.Web.Areas.Director.Models.ApplicationsViewModels
+{
+    using Data.Models;
+    using Infrastructure.Mappings;
+
+    public class MajorViewModel : IMapFrom<Major>
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Source/Web/Interapp.Web/Areas/Director/Models/ApplicationsViewModels/ResponseViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Interapp.Web.Areas.Director.Models.ApplicationsViewModels
+{
+    using Data.Models;
+    using Infrastructure.Mappings;
+
+    public class ResponseViewModel : IMapFrom<Response>
+    {
+        // TODO
+    }
+}
