e62423ff80aa1203c9368673740d05f6ed052fc3
shunobaka <vesselin_minev@abv.bg>
2016-02-17 19:41:41 +0200

    Implemented view and logic for listing responses

--- Source/Web/Interapp.Web/Areas/Student/Models/ResponsesViewModels/ResponseViewModel.cs
+++ Source/Web/Interapp.Web/Areas/Student/Models/ResponsesViewModels/ResponseViewModel.cs
@@ -2,12 +2,17 @@
 {
     using Data.Models;
     using Infrastructure.Mappings;
+    using System;
 
     public class ResponseViewModel : IMapFrom<Response>
     {
+        public int ApplicationId { get; set; }
+
         public string Content { get; set; }
 
         public bool IsAdmitted { get; set; }
+
+        public DateTime? Date { get; set; }
 
         public ApplicationViewModel Application { get; set; }
     }
