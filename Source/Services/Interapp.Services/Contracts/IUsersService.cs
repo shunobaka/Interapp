eb2dd61299687c0fb3a2b84733312c8193944d4a
shunobaka <vesselin_minev@abv.bg>
2016-02-18 16:24:21 +0200

    Implemented UsersService

---
+++ Source/Services/Interapp.Services/Contracts/IUsersService.cs
@@ -0,0 +1,16 @@
+﻿namespace Interapp.Services.Contracts
+{
+    using Data.Models;
+    using System.Linq;
+
+    public interface IUsersService
+    {
+        IQueryable<User> All();
+
+        User GetById(string id);
+
+        void Update(User user);
+
+        void Delete(string id);
+    }
+}
