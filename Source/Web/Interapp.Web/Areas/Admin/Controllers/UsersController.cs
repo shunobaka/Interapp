e6534c2fd9bcbed090521aff2de1e2db1b3e0332
shunobaka <vesselin_minev@abv.bg>
2016-02-18 16:16:59 +0200

    Added scaffold grid for editing, creating, deleting users

---
+++ Source/Web/Interapp.Web/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,162 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using Interapp.Data.Models;
+
+namespace Interapp.Web.Areas.Admin
+{
+    public class UsersController : Controller
+    {
+        private IUsersRepository users;
+
+        public UsersController(IUsersRepository users)
+        {
+            this.users = users;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Users_Read([DataSourceRequest]DataSourceRequest request)
+        {
+            IQueryable<User> users = db.Users;
+            DataSourceResult result = users.ToDataSourceResult(request, user => new {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                DateOfBrith = user.DateOfBrith,
+                DirectorInfoId = user.DirectorInfoId,
+                StudentInfoId = user.StudentInfoId,
+                Email = user.Email,
+                EmailConfirmed = user.EmailConfirmed,
+                PasswordHash = user.PasswordHash,
+                SecurityStamp = user.SecurityStamp,
+                PhoneNumber = user.PhoneNumber,
+                PhoneNumberConfirmed = user.PhoneNumberConfirmed,
+                TwoFactorEnabled = user.TwoFactorEnabled,
+                LockoutEndDateUtc = user.LockoutEndDateUtc,
+                LockoutEnabled = user.LockoutEnabled,
+                AccessFailedCount = user.AccessFailedCount,
+                UserName = user.UserName
+            });
+
+            return Json(result);
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Users_Create([DataSourceRequest]DataSourceRequest request, User user)
+        {
+            if (ModelState.IsValid)
+            {
+                var entity = new User
+                {
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    DateOfBrith = user.DateOfBrith,
+                    DirectorInfoId = user.DirectorInfoId,
+                    StudentInfoId = user.StudentInfoId,
+                    Email = user.Email,
+                    EmailConfirmed = user.EmailConfirmed,
+                    PasswordHash = user.PasswordHash,
+                    SecurityStamp = user.SecurityStamp,
+                    PhoneNumber = user.PhoneNumber,
+                    PhoneNumberConfirmed = user.PhoneNumberConfirmed,
+                    TwoFactorEnabled = user.TwoFactorEnabled,
+                    LockoutEndDateUtc = user.LockoutEndDateUtc,
+                    LockoutEnabled = user.LockoutEnabled,
+                    AccessFailedCount = user.AccessFailedCount,
+                    UserName = user.UserName
+                };
+
+                db.Users.Add(entity);
+                db.SaveChanges();
+                user.Id = entity.Id;
+            }
+
+            return Json(new[] { user }.ToDataSourceResult(request, ModelState));
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Users_Update([DataSourceRequest]DataSourceRequest request, User user)
+        {
+            if (ModelState.IsValid)
+            {
+                var entity = new User
+                {
+                    Id = user.Id,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    DateOfBrith = user.DateOfBrith,
+                    DirectorInfoId = user.DirectorInfoId,
+                    StudentInfoId = user.StudentInfoId,
+                    Email = user.Email,
+                    EmailConfirmed = user.EmailConfirmed,
+                    PasswordHash = user.PasswordHash,
+                    SecurityStamp = user.SecurityStamp,
+                    PhoneNumber = user.PhoneNumber,
+                    PhoneNumberConfirmed = user.PhoneNumberConfirmed,
+                    TwoFactorEnabled = user.TwoFactorEnabled,
+                    LockoutEndDateUtc = user.LockoutEndDateUtc,
+                    LockoutEnabled = user.LockoutEnabled,
+                    AccessFailedCount = user.AccessFailedCount,
+                    UserName = user.UserName
+                };
+
+                db.Users.Attach(entity);
+                db.Entry(entity).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+
+            return Json(new[] { user }.ToDataSourceResult(request, ModelState));
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Users_Destroy([DataSourceRequest]DataSourceRequest request, User user)
+        {
+            if (ModelState.IsValid)
+            {
+                var entity = new User
+                {
+                    Id = user.Id,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    DateOfBrith = user.DateOfBrith,
+                    DirectorInfoId = user.DirectorInfoId,
+                    StudentInfoId = user.StudentInfoId,
+                    Email = user.Email,
+                    EmailConfirmed = user.EmailConfirmed,
+                    PasswordHash = user.PasswordHash,
+                    SecurityStamp = user.SecurityStamp,
+                    PhoneNumber = user.PhoneNumber,
+                    PhoneNumberConfirmed = user.PhoneNumberConfirmed,
+                    TwoFactorEnabled = user.TwoFactorEnabled,
+                    LockoutEndDateUtc = user.LockoutEndDateUtc,
+                    LockoutEnabled = user.LockoutEnabled,
+                    AccessFailedCount = user.AccessFailedCount,
+                    UserName = user.UserName
+                };
+
+                db.Users.Attach(entity);
+                db.Users.Remove(entity);
+                db.SaveChanges();
+            }
+
+            return Json(new[] { user }.ToDataSourceResult(request, ModelState));
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Source/Web/Interapp.Web/Areas/Admin/Models/UsersViewModels/UserViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Interapp.Web.Areas.Admin.Models.UsersViewModels
+{
+    using Infrastructure.Mappings;
+    using Data.Models;
+
+    public class UserViewModel : IMapFrom<User>
+    {
+
+    }
+}
