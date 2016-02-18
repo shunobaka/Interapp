e589ad940c9d4d48bc141d6e626f8aeca6b99d0b
shunobaka <vesselin_minev@abv.bg>
2016-02-18 19:42:40 +0200

    Implemented full functioning grid with CRUD operations for majors

---
+++ Source/Web/Interapp.Web/Areas/Admin/Models/MajorsViewModels/MajorViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Interapp.Web.Areas.Admin.Models.MajorsViewModels
+{
+    using Infrastructure.Mappings;
+    using Data.Models;
+    using System.ComponentModel.DataAnnotations;
+    using Common.Constants;
+
+    public class MajorViewModel : IMapFrom<Major>
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(ModelConstants.MajorNameMinLength)]
+        [MaxLength(ModelConstants.MajorNameMaxLength)]
+        public string Name { get; set; }
+    }
+}
