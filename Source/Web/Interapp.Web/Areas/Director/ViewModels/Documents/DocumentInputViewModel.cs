9724c6c2d3eae569dff3a8a726ccc9d84c045429
shunobaka <vesselin_minev@abv.bg>
2016-02-21 17:12:39 +0200

    Implemented models and views for listing and adding documents

---
+++ Source/Web/Interapp.Web/Areas/Director/ViewModels/Documents/DocumentInputViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace Interapp.Web.Areas.Director.ViewModels.Documents
+{
+    using System.ComponentModel.DataAnnotations;
+    using Common.Constants;
+
+    public class DocumentInputViewModel
+    {
+        [Required]
+        [Display(Name = "University")]
+        public int UniversityId { get; set; }
+
+        [Required]
+        [MinLength(
+            ModelConstants.DocumentNameMinLength,
+            ErrorMessage = ModelErrorMessages.MinLengthErrorMessage)]
+        [MaxLength(
+            ModelConstants.DocumentNameMaxLength,
+            ErrorMessage = ModelErrorMessages.MaxLengthErrorMessage)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+    }
+}
