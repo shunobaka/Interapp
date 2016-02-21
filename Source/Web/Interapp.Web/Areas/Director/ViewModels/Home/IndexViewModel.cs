70c5522231cd67be217ec308daf898f134127613
shunobaka <vesselin_minev@abv.bg>
2016-02-21 19:08:54 +0200

    Implemented paging for universities on Director's main page

---
+++ Source/Web/Interapp.Web/Areas/Director/ViewModels/Home/IndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Interapp.Web.Areas.Director.ViewModels.Home
+{
+    using System.Collections.Generic;
+
+    public class IndexViewModel
+    {
+        public IList<UniversityViewModel> Universities { get; set; }
+
+        public int UniversitiesCount { get; set; }
+
+        public int Page { get; set; }
+
+        public int PageSize { get; set; }
+    }
+}
